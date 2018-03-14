b34a0ec145ee342bf5ec393378c6d08215b6350c
User <user@MacBooks-MacBook-Air.local>
2018-03-14 11:31:25 -0700

    Added start of question one.

---
+++ src/lang.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    bool useLang1, useLang2 = false;
+    bool goodLang1, goodLang2 = true;
+
+    string input;
+
+    getline(cin,input);
+
+    return 0;
+}
